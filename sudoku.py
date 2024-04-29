ac3517324f76d57692754f5f7694d5f923ae64eb
its-mmil <mark_e_miller@brown.edu>
2024-04-29 14:12:21 -0400

    implemented naive guessing strategy

--- sudoku.py
+++ sudoku.py
@@ -16,7 +16,7 @@ def print_grid(grid):
 
     for row in grid:
         for col in row:
-            print(col, end=" ")
+            print(col if col != -1 else "_", end=" ")
         print()
 
 class Sudoku(object):
@@ -29,87 +29,137 @@ class Sudoku(object):
         # Solver
         self.s = Solver()
 
-        # Grid Variables
-        self.L = {(i, j): Int('var_{}_{}'.format(i, j)) for i in range(N) for j in range(N)}
+        self.board_count = 0
 
-
+    def solve(self, board):
+        result = self.s.check()
+        if result == sat:
+            return get_grid(self.N, board, self.s.model())
+        else:
+            return None
+    
     def get_3x3_section(self, row, col):
-        start_row = (row // 3) * 3
-        start_col = (col // 3) * 3
-        section = []
-        for i in range(start_row, start_row + 3):
-            section.append(self.L[(i,start_col)])
-            section.append(self.L[(i,start_col +1)])
-            section.append(self.L[(i,start_col +2)])
-        return section
-
-    def solve(self):
-
-        # EDIT HERE: PART 1 CONSTRAINTS
-        # Here, `self.N` is the size of the board, and `self.game_data` is the game data
-        # Feel free to use `self.s.push()` and `self.s.pop()` across `solve` and `check_multiple`
-        # to reset constraints if you don't want to reuse them in `check_multiple`
-
+        return (row // 3) * 3 + (col // 3)
+    
+    def create_board(self, known_cells = [], no_fill = True):
+        self.board_count += 1
+        board = {(i, j): Int('{}_{}_{}'.format(self.board_count, i, j)) for i in range(self.N) for j in range(self.N)}
+        
+        # Establish board constraints
         for i in range(self.N):
             for j in range(self.N):
-                self.s.add(self.L[(i, j)] >0)
-                self.s.add(self.L[(i, j)] <self.N+1)
+                self.s.add(Or(
+                    board[(i, j)] == -1, 
+                    And(board[(i, j)] > 0, board[(i, j)] < self.N + 1)))
 
-        # constraining rows
-        for row in range(self.N):
-            for i in range(self.N):
-                for j in range(self.N):
-                    if (i !=j):
-                        self.s.add(self.L[(row, i)] != self.L[(row, j)])
-
-        # constraining cols
-        for col in range(self.N):
-            for i in range(self.N):
-                for j in range(self.N):
-                    if (i !=j):
-                        self.s.add(self.L[(i, col)] != self.L[(j, col)])
-
-        # constraining sections
-        # this could probably be cut down time-wise
         for i in range(self.N):
             for j in range(self.N):
                 for k in range(self.N):
                     for l in range(self.N):
-                        if (i!=k or j!=l):
-                            if (self.get_3x3_section(i, j) ==
-                                self.get_3x3_section(k, l)):
-                                self.s.add(self.L[(i, j)] != self.L[(k, l)])
+                        if i == k and j == l:
+                            continue
+                        if i == k or j == l or self.get_3x3_section(i, j) == self.get_3x3_section(k, l):
+                            self.s.add(Or(
+                                board[(i, j)] == -1, board[(k, l)] == -1, 
+                                board[(i, j)] != board[(k, l)]))
 
+        # Insert inputted values
+        for r, row in enumerate(known_cells):
+            for c, num in enumerate(row):
+                num_to_insert = num if num is not None else -1
+                if no_fill or num_to_insert != -1:
+                    self.s.add(board[(r, c)] == num_to_insert)
 
+        return board
+        
+    def generate_solved_board(self, known_cells = []):
+        board = self.create_board(known_cells, False)
+        for r in range(self.N):
+            for c in range(self.N):
+                self.s.add(board[(r, c)] != -1)
+        return self.solve(board)
 
-        # Check for PART 1
-        result = self.s.check()
-        if result == sat:
-            return get_grid(self.N, self.L, self.s.model())
-        else:
-            return None
+    def guess_cell(self, pre, post):
+        constraints = []
+        for i in range(self.N):
+            for j in range(self.N):
+                self.s.add(Implies(pre[(i, j)] != -1, pre[(i, j)] == post[(i, j)]))
+                constraints.append(pre[(i, j)] != post[(i, j)])
+        # Constraint enforcing that exactly one square has changed
+        self.s.add(PbEq([(x,1) for x in constraints], 1))
 
+def get_board_difference(board1, board2):
+    for r, (row1, row2) in enumerate(zip(board1, board2)):
+        for c, (val1, val2) in enumerate(zip(row1, row2)):
+            if val1 != val2:
+                return (r, c)
 
-    def check_multiple(self, ans):
-        """PART 2"""
+def apply_strategy(sudoku, initial, guesses=[], constraint_map=[]): #add fill in strategy parameter later
+    sudoku.s.reset()
+    pre = sudoku.create_board(initial)
+    pre_board = sudoku.solve(pre)
+    if not any([-1 in row for row in pre_board]):
+        print("terminated by solving")
+        return steps # Board already solved
+    post = sudoku.create_board()
 
-        # EDIT HERE: PART 2 CONSTRAINTS
-        # Here, `self.N` is the size of the board, and `self.game_data` is the game data
-        # Feel free to use `self.s.push()` and `self.s.pop()` across `solve` and `check_multiple`
-        # to reset constraints if you don't want to reuse them in `check_multiple`
+    # Add any constraints banning failed guessed values of squares
+    for _, r, c, bad_value in constraint_map:
+        sudoku.s.add(post[(r, c)] != bad_value)
 
-        self.s.add(ans != solve())
-        # Check for PART 2
-        result = self.s.check()
-        return result == sat
+    steps = 1
+    sudoku.guess_cell(pre, post)
+    post_board = sudoku.solve(post)
+    main(pre_board)
+    while post_board is not None:
+        pre_board = sudoku.solve(pre)
+        difference = get_board_difference(pre_board, post_board)
+        main(post_board, [difference])
+        if not any([-1 in row for row in post_board]):
+            print("terminated by solving")
+            return steps # board solved
+        guesses.append((difference, post_board[difference[0]][difference[1]]))
+        pre = sudoku.create_board(post_board)
+        post = sudoku.create_board()
+        steps += 1
+        sudoku.guess_cell(pre, post)
+        post_board = sudoku.solve(post)
+
+    # Dead end reached if while loop terminates, so backtrack
+    if len(guesses) == 0:
+        print("terminated with no solution")
+        return steps # Cannot backtrack, so no solution
+    (last_r, last_c), last_guess = guesses.pop()
+    # Remove no longer relevant constraints
+    while constraint_map and len(guesses) < constraint_map[-1][0]:
+        constraint_map.pop() 
+    # Add constraint banning the last guessed value for that square 
+    constraint_map.append((len(guesses), last_r, last_c, last_guess))
+    # Remove last guess and try to proceed again
+    pre_board[last_r][last_c] = -1
+    return steps + apply_strategy(sudoku, pre_board, guesses, constraint_map)
+    
 
 if __name__ == "__main__":
     game_data_example = [
-      [[' ' for _ in range(9)] for _ in range(9)]
+        [1, None, None, None, None, None, 8, None, None],
+        [None, None, 4, 2, None, None, None, None, 7],
+        [None, 3, None, None, None, None, None, None, 9],
+        [None, 9, None, None, 6, 7, None, None, None],
+        [None, None, None, None, 5, 1, None, None, 2],
+        [6, None, None, 9, None, None, None, 1, None],
+        [None, None, 8, 5, None, None, None, None, None],
+        [None, None, 6, None, None, None, None, None, None],
+        [None, 7, None, None, None, 3, 1, 9, None]
     ]
+
+    #one from solved example
+    # game_data_example = [[5, None, 7, 6, 9, 8, 2, 3, 4], [2, 8, 9, 1, 3, 4, 7, 5, 6], [3, 4, 6, 2, 7, 5, 8, 9, 1], [6, 7, 2, 8, 4, 9, 3, 1, 5], [1, 3, 8, 5, 2, 6, 9, 4, 7], [9, 5, 4, 7, 1, 3, 6, 8, 2], [4, 9, 5, 3, 6, 2, 1, 7, 8], [7, 2, 3, 4, 8, 1, 5, 6, 9], [8, 6, 1, 9, 5, 7, 4, 2, 3]]
+    
+    # backtracking example
+    game_data_example = [[5, 1, 7, 6, 9, 8, 2, None, 4], [2, 8, 9, 1, None, None, 7, None, 6], [3, 4, 6, 2, 7, 5, 8, 9, 1], [6, 7, 2, 8, 4, 9, 3, 1, 5], [1, 3, 8, 5, 2, 6, 9, 4, 7], [9, 5, 4, 7, 1, 3, 6, 8, 2], [4, 9, 5, 3, 6, 2, 1, 7, 8], [7, 2, 3, 4, 8, 1, 5, 6, 9], [8, 6, 1, 9, 5, 7, 4, 2, 3]]
 
     sudoku = Sudoku(9, game_data_example)
 
-    ans = sudoku.solve()
-    main(ans)
-    print_grid(ans)
+    steps_taken = apply_strategy(sudoku, game_data_example)
+    print(f"Steps: {steps_taken}")
--- visualizer.py
+++ visualizer.py
@@ -7,15 +7,16 @@ TOTAL_SIZE = NUM_SQUARES * SQUARE_SIZE
 WHITE = (255,255,255)
 GRAY  = (200,200,200)
 BLACK = (0,0,0)
+RED = (255,0,0)
 
-def draw_board(screen, board):
+def draw_board(screen, board, highlights):
     screen.fill(WHITE)
     for y in range(NUM_SQUARES):
         for x in range(NUM_SQUARES):
-            value = board[y][x]
+            value = board[y][x] if board[y][x] != -1 else None
             if value:
                 font = pygame.font.Font(size=36)
-                text = font.render(str(value), True, BLACK)
+                text = font.render(str(value), True, RED if (y,x) in highlights else BLACK)
                 text_rect = text.get_rect(center=((x+.5)*SQUARE_SIZE, (y+.5)*SQUARE_SIZE))
                 screen.blit(text, text_rect)
 
@@ -29,7 +30,9 @@ def draw_board(screen, board):
         pygame.draw.line(screen, BLACK, (0, offset*SQUARE_SIZE), (TOTAL_SIZE, offset*SQUARE_SIZE), 4)
 
 
-def main(board):
+def main(board, highlights = []):
+    if board is None:
+        return
     pygame.init()
     screen = pygame.display.set_mode((TOTAL_SIZE, TOTAL_SIZE))
     clock = pygame.time.Clock()
@@ -39,7 +42,7 @@ def main(board):
             if event.type == pygame.QUIT:
                 running = False
 
-        draw_board(screen, board)
+        draw_board(screen, board, highlights)
 
         pygame.display.flip()
         clock.tick(50)

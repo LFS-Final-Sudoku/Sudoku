e324f4a0736e7b751dbf9cc3b5fa7c4b751d6323
Mahlon Page <mahlon_page@brown.edu>
2024-05-06 13:33:14 -0400

    commenting

--- sudoku.py
+++ sudoku.py
@@ -21,33 +21,44 @@ def print_grid(grid):
 
 class Sudoku(object):
     def __init__(self, N):
+        """
+        Constructor of this class
+        """
         assert int(N ** (1/2)) == N ** (1/2)
-        self.N = N
+        self.N = N # Size of the board
         self.s = Solver()
         self.board_count = 0
 
     def solve(self, board):
+        """
+        Returns the solved board if SAT or returns nothing if it is not.
+        """
         result = self.s.check()
-        if result == sat:
-            return get_grid(self.N, board, self.s.model())
-        else:
-            return None
-    
+        if result == sat: return get_grid(self.N, board, self.s.model())
+        else:             return None
+
     def get_mini_section(self, row, col):
+        """
+        Determine which subgrid a coordinate is in.
+        """
         section_size = self.N ** (1/2)
         return (row // section_size) * section_size + (col // section_size)
-    
+
     def create_board(self, known_cells = [], no_fill = True):
+        """
+        Generates an empty board and adds wellformedness rules for sudoku.
+        """
         self.board_count += 1
         board = {(i, j): Int('{}_{}_{}'.format(self.board_count, i, j)) for i in range(self.N) for j in range(self.N)}
-        
+
         # Establish board constraints
         for i in range(self.N):
             for j in range(self.N):
                 self.s.add(Or(
-                    board[(i, j)] == -1, 
+                    board[(i, j)] == -1,
                     And(board[(i, j)] > 0, board[(i, j)] < self.N + 1)))
 
+        # Establish uniqueness within row, column, and subgrid.
         for i in range(self.N):
             for j in range(self.N):
                 for k in range(self.N):
@@ -56,7 +67,7 @@ class Sudoku(object):
                             continue
                         if i == k or j == l or self.get_mini_section(i, j) == self.get_mini_section(k, l):
                             self.s.add(Or(
-                                board[(i, j)] == -1, board[(k, l)] == -1, 
+                                board[(i, j)] == -1, board[(k, l)] == -1,
                                 board[(i, j)] != board[(k, l)]))
 
         # Insert inputted values
@@ -65,7 +76,7 @@ class Sudoku(object):
                 num_to_insert = num if num is not None else -1
                 if no_fill or num_to_insert != -1:
                     self.s.add(board[(r, c)] == num_to_insert)
-        
+
         # Create possibilities map
         possibilities = {(i, j): [Bool('{} {}_{}_{}'.format(num, self.board_count, i, j)) for num in range(self.N)] for i in range(self.N) for j in range(self.N)}
         for i in range(self.N):
@@ -83,7 +94,7 @@ class Sudoku(object):
                         possibilities[(i, j)][num - 1] == False,
                         possibilities[(i, j)][num - 1] == True
                     ))
-        
+
         #Count possibilities per square
         possibility_counts = {(i, j): Int('counts {}_{}_{}'.format(self.board_count, i, j)) for i in range(self.N) for j in range(self.N)}
         for i in range(self.N):
@@ -93,10 +104,13 @@ class Sudoku(object):
                         PbEq([(var, 1) for var in possibilities[(i, j)]], count),
                         possibility_counts[(i, j)] == count
                     ))
-                            
+
         return board, possibility_counts
-        
+
     def generate_solved_board(self, known_cells = []):
+        """
+        Generates a solved board.
+        """
         board, _ = self.create_board(known_cells, False)
         for r in range(self.N):
             for c in range(self.N):
@@ -104,6 +118,9 @@ class Sudoku(object):
         return self.solve(board)
 
     def guess_cell(self, pre, post, _):
+        """
+        Takes a guess for one cell on the grid.
+        """
         constraints = []
         for i in range(self.N):
             for j in range(self.N):
@@ -113,6 +130,9 @@ class Sudoku(object):
         self.s.add(PbEq([(x,1) for x in constraints], 1))
 
     def guess_least_possibilities(self, pre, post, possibility_counts):
+        """
+        Find a cell with the least number of possible options and begin guessing there.
+        """
         self.guess_cell(pre, post, possibility_counts) # Exactly one blank cell must be filled
         # Variable to store minimum possibilities count among blank spaces
         min_possibilities = Int(f"min_possibilities {self.board_count}")
@@ -127,12 +147,15 @@ class Sudoku(object):
                 ))
 
     def possible_values(self, row, col, post):
-        # Base case: check if it is UNSAT, and if so, return 0
+        """
+        Recursively count the number of possible values for a cell.
+        """
+        # Base case: if UNSAT, return 0
         if self.solve(post) is None:
             return 0
 
         value = post[(row, col)]
-        
+
         # Push so that we can (temporarily) add a constraint
         self.s.push()
         # Add the constraint that the value is actually different
@@ -144,6 +167,9 @@ class Sudoku(object):
         return count + 1
 
 def remove_values(board, num_to_remove):
+    """
+    Removes random values from a solved sudoku board to generate an unsolved board.
+    """
     board_copy = [row.copy() for row in board]
     filled_locations = []
     for r, row in enumerate(board_copy):
@@ -156,6 +182,9 @@ def remove_values(board, num_to_remove):
     return board_copy
 
 def generate_random_starting_board(N, num_unfilled):
+    """
+    Generates a random starting board of size N with num_unfilled empty squares.
+    """
     sudoku = Sudoku(N)
     random_row = sample(range(1, N + 1), N)
     known_cells = [[-1 for _ in range(N)] for _ in range(N)]
@@ -164,12 +193,18 @@ def generate_random_starting_board(N, num_unfilled):
     return remove_values(board, num_unfilled)
 
 def get_board_difference(board1, board2):
+    """
+    Finds the location of difference in two boards.
+    """
     for r, (row1, row2) in enumerate(zip(board1, board2)):
         for c, (val1, val2) in enumerate(zip(row1, row2)):
             if val1 != val2:
                 return (r, c)
 
 def apply_strategy(sudoku, initial, guess_strategy, guesses=[], constraint_map=[], steps = 0, max_steps = 60, use_visualizer = True):
+    """
+    Applies a Sudoku solving strategy.
+    """
     sudoku.s.reset()
     pre, pre_possibility_counts = sudoku.create_board(initial)
     pre_board = sudoku.solve(pre)
@@ -181,29 +216,38 @@ def apply_strategy(sudoku, initial, guess_strategy, guesses=[], constraint_map=[
     # Add any constraints banning failed guessed values of squares
     for _, r, c, bad_value in constraint_map:
         sudoku.s.add(post[(r, c)] != bad_value)
+
     steps += 1
     if steps > max_steps:
         print("out of steps")
         return steps
+
     guess_strategy(pre, post, pre_possibility_counts)
     post_board = sudoku.solve(post)
+
     if use_visualizer:
         visualize(pre_board)
+
     while post_board is not None:
         pre_board = sudoku.solve(pre)
         difference = get_board_difference(pre_board, post_board)
+
         if use_visualizer:
             visualize(post_board, [difference])
+
         if not any([-1 in row for row in post_board]):
             print("terminated by solving")
             return steps # board solved
+
         guesses.append((difference, post_board[difference[0]][difference[1]]))
         pre, pre_possibility_counts = sudoku.create_board(post_board)
         post, _ = sudoku.create_board()
+
         steps += 1
         if steps > max_steps:
             print("out of steps")
             return steps
+
         guess_strategy(pre, post, pre_possibility_counts)
         post_board = sudoku.solve(post)
 
@@ -215,9 +259,9 @@ def apply_strategy(sudoku, initial, guess_strategy, guesses=[], constraint_map=[
 
     # Remove no longer relevant constraints
     while constraint_map and len(guesses) < constraint_map[-1][0]:
-        constraint_map.pop() 
+        constraint_map.pop()
 
-    # Add constraint banning the last guessed value for that square 
+    # Add constraint banning the last guessed value for that square
     constraint_map.append((len(guesses), last_r, last_c, last_guess))
 
     # Remove last guess and try to proceed again
@@ -225,6 +269,9 @@ def apply_strategy(sudoku, initial, guess_strategy, guesses=[], constraint_map=[
     return apply_strategy(sudoku, pre_board, guess_strategy, guesses, constraint_map, steps, max_steps, use_visualizer)
 
 def time_strategy(sudoku, guess_strategy, trials, num_unfilled, max_steps_per_trial):
+    """
+    Generates random boards and counts the number of steps needed to solve, returning an average timesteps to solve.
+    """
     trial_steps = []
     while len(trial_steps) < trials:
         board = generate_random_starting_board(sudoku.N, num_unfilled)
@@ -248,13 +295,13 @@ if __name__ == "__main__":
 
     #one from solved example
     # game_data_example = [[5, None, 7, 6, 9, 8, 2, 3, 4], [2, 8, 9, 1, 3, 4, 7, 5, 6], [3, 4, 6, 2, 7, 5, 8, 9, 1], [6, 7, 2, 8, 4, 9, 3, 1, 5], [1, 3, 8, 5, 2, 6, 9, 4, 7], [9, 5, 4, 7, 1, 3, 6, 8, 2], [4, 9, 5, 3, 6, 2, 1, 7, 8], [7, 2, 3, 4, 8, 1, 5, 6, 9], [8, 6, 1, 9, 5, 7, 4, 2, 3]]
-    
+
     # backtracking example
     # game_data_example = [[5, 1, 7, 6, 9, 8, 2, None, 4], [2, 8, 9, 1, None, None, 7, None, 6], [3, 4, 6, 2, 7, 5, 8, 9, 1], [6, 7, 2, 8, 4, 9, 3, 1, 5], [1, 3, 8, 5, 2, 6, 9, 4, 7], [9, 5, 4, 7, 1, 3, 6, 8, 2], [4, 9, 5, 3, 6, 2, 1, 7, 8], [7, 2, 3, 4, 8, 1, 5, 6, 9], [8, 6, 1, 9, 5, 7, 4, 2, 3]]
 
     # sudoku = Sudoku(9)
-    # apply_strategy(sudoku, game_data_example, sudoku.guess_least_possibilities) 
-     
+    # apply_strategy(sudoku, game_data_example, sudoku.guess_least_possibilities)
+
     sudoku = Sudoku(4)
     average_steps = time_strategy(sudoku, sudoku.guess_least_possibilities, 5, 10, 100)
     print(f"Average steps: {average_steps}")
